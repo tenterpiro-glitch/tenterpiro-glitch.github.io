05f81de8a41debe599ee2cfb504fde7ce5cfaa74
tenterpiro-glitch <tenterpiro@gmail.com>
2025-09-10 09:59:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const copyBtn = document.querySelector("#download .btn");
-copyBtn.addEventListener("click", () => {
-  navigator.clipboard.writeText("play.tentermc.fun");
-  copyBtn.textContent = "IP Copied!";
-  setTimeout(() => { copyBtn.textContent = "Copy IP"; }, 2000);
-});
